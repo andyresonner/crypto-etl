2de2bdecd28bfcaf49356f17bf713617eb3030ff
andyresonner <mxsupps@gmail.com>
2025-06-12 13:43:58 -0700

    Update __init__.py

--- crypto_etl/__init__.py
+++ crypto_etl/__init__.py
@@ -1,1 +1,6 @@
-from . import db      # expose the helper so “from . import db” works
+"""
+crypto_etl package
+
+(Empty __init__ so that `import crypto_etl.db` works without
+triggering a circular import during GitHub Actions runs.)
+"""
